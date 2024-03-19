3545cceeb1949c7a5d37acf0182669c75ae37cbd
Ibi_Tea <hyvak52@gmail.com>
2024-03-19 22:29:18 +0300

    Revert "Update Catwar UwU.js"
    This reverts commit ed8ce8caf51e5158525a57105ed87666f503572e.

--- Catwar UwU.js
+++ Catwar UwU.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Catwar UwU
 // @namespace    http://tampermonkey.net/
-// @version      v1.5.4-03.24
+// @version      v1.5.3-03.24
 // @description  Визуальное обновление CatWar'а.
 // @author       Ibirtem / Тенёчек ( https://catwar.su/cat1477928 )
 // @match        http*://*.catwar.su/*
@@ -15,6 +15,7 @@
 // Вроде как начальные настройки.
 const targetSettings = "https://catwar.su/settings";
 const targetCW3 = "https://catwar.su/cw3/";
+const weatherSettings = "true";
 // ====================================================================================================================
 
 // ====================================================================================================================
@@ -93,45 +94,55 @@ let css = `
   pointer-events: none;
 }
 
-.northern-light {
-  filter: blur(100px);
+.rainCanvas {
+  z-index: 1;
+  position: absolute;
+}
+
+.snowCanvas {
+  z-index: 2;
+  position: absolute;
 }
 `;
 GM_addStyle(css);
 // ====================================================================================================================
 
 // ====================================================================================================================
-function createSettingsBlock(blockId, settings) {
-  const settingsContainer = document.querySelector("#branch");
-  const backgroundImageURL = window.getComputedStyle(document.body).backgroundImage;
-
-  const settingsElement = document.createElement("div");
-  settingsElement.classList.add("rounded-image");
-  settingsElement.id = blockId;
-  settingsElement.innerHTML = settings;
-  settingsElement.style.backgroundImage = backgroundImageURL;
-
-  settingsContainer.appendChild(settingsElement);
-}
-
-function handleCheckboxState(checkboxId, localStorageKey, defaultValue) {
-  const checkbox = document.querySelector(`#${checkboxId}`);
-
-  // Получаем значение из localStorage или используем defaultValue
-  const storedValue = localStorage.getItem(localStorageKey) || defaultValue;
-  checkbox.checked = storedValue === "true";
-
-  // Обработчик изменения состояния чекбокса
-  checkbox.addEventListener("change", () => {
-    const newValue = checkbox.checked ? "true" : "false";
-    localStorage.setItem(localStorageKey, newValue);
-  });
-}
-
-// Новые чекбоксы и вставка настроек
+// Вставка настроек в настройки пока мы совершенно точно в настройках. Да? Да ведь?
 if (window.location.href === targetSettings) {
-  createSettingsBlock("uwu-settings", uwusettings);
-  handleCheckboxState("weather-enabled", "weatherSettings", "true");
+  const settings = document.querySelector("#branch");
+  const backgroundImageURL = window.getComputedStyle(
+    document.body
+  ).backgroundImage;
+
+  const uwusettingsElement = document.createElement("div");
+  uwusettingsElement.classList.add("rounded-image");
+  uwusettingsElement.innerHTML = uwusettings;
+  uwusettingsElement.style.backgroundImage = backgroundImageURL;
+
+  settings.appendChild(uwusettingsElement);
+
+  // Погода в настройках о господе сколько тут текста бесполезного будет
+  const weatherSettingsCheckbox = document.querySelector("#weather-enabled");
+
+  if (!localStorage.getItem("weatherSettings")) {
+    localStorage.setItem("weatherSettings", weatherSettings);
+    // Костыль значения checkbox
+    weatherSettingsCheckbox.checked = weatherSettings === "true";
+  } else {
+    const weatherSettingsFromLocalStorage =
+      localStorage.getItem("weatherSettings");
+    weatherSettingsCheckbox.checked =
+      weatherSettingsFromLocalStorage === "true";
+  }
+  // Вертим checkbox на хахахахаха
+  weatherSettingsCheckbox.addEventListener("change", () => {
+    const newWeatherSettings = weatherSettingsCheckbox.checked
+      ? "true"
+      : "false";
+    localStorage.setItem("weatherSettings", newWeatherSettings);
+    console.log(weatherSettings);
+  });
 }
 // ====================================================================================================================
 
@@ -143,7 +154,7 @@ if (window.location.href === targetCW3) {
   globalContainerElement.id = "global-container";
   containerElement.appendChild(globalContainerElement);
   // ====================================================================================================================
-  var weather = "null";
+  var weather = "clear";
 
   function getSkyType() {
     const skyElement = document.querySelector("#sky");
@@ -172,23 +183,24 @@ if (window.location.href === targetCW3) {
 
   // ====================================================================================================================
   const weatherContainer = document.getElementById("global-container");
-  const weatherCanvas = document.createElement("canvas");
-  weatherCanvas.classList.add("weatherCanvas");
-  weatherContainer.appendChild(weatherCanvas);
-  const weatherCtx = weatherCanvas.getContext("2d");
-  
-  function resizeCanvasElement() {
-    weatherCanvas.width = weatherCanvas.parentNode.offsetWidth;
-    weatherCanvas.height = weatherCanvas.parentNode.offsetHeight;
+
+  function generateWeather() {
+    // бляяя я уже сам не знаю чё делаю но TODO объединить генерацию частиц в один canvas и удалять его когда небо чисто а то я объебался и теперь не ебу как это сделать я слишком тупой и ленивый спасите как я вообще это ещё делаю
+    const weatherCanvas = document.createElement("canvas");
+    weatherCanvas.classList.add("weatherCanvas");
+
+    weatherContainer.appendChild(weatherCanvas);
+
+    return { weatherCanvas };
   }
-  
-  window.addEventListener("resize", resizeCanvasElement);
-  resizeCanvasElement();
-  
-  // ====================================================================================================================
+
+  // Генератор слёз и боли
   function generateRain() {
     const raindrops = [];
-  
+    const rainCanvas = document.createElement("canvas");
+    rainCanvas.classList.add("rainCanvas");
+    const rainParticle = rainCanvas.getContext("2d");
+
     setInterval(() => {
       for (let i = 0; i < 12; i++) {
         const raindrop = weather === "rain" ? generateRaindrop() : null;
@@ -197,24 +209,26 @@ if (window.location.href === targetCW3) {
         }
       }
     }, 60);
-  
+
+    // Генерируем каплю
     function generateRaindrop() {
       if (document.hidden) {
         return;
       }
-      const x = Math.random() * weatherCanvas.width;
+      const x = Math.random() * rainCanvas.width;
       const y = Math.random() * -100;
       const length = Math.random() * 10 + 50;
       const width = Math.random() * 1 + 1;
       const ySpeed = length * 0.2;
       const xSpeed = Math.random() * 1;
-  
+
       return { x, y, length, width, ySpeed, xSpeed };
     }
-  
+
+    // Анимация капель
     function animateRain() {
       if (raindrops.length > 0) {
-        weatherCtx.clearRect(0, 0, weatherCanvas.width, weatherCanvas.height);
+        rainParticle.clearRect(0, 0, rainCanvas.width, rainCanvas.height);
         for (const raindrop of raindrops) {
           raindrop.y += raindrop.ySpeed;
           raindrop.x += raindrop.xSpeed;
@@ -223,10 +237,11 @@ if (window.location.href === targetCW3) {
       }
       requestAnimationFrame(animateRain);
     }
-  
+
+    // Рисуем каплю
     function drawRaindrop(raindrop) {
-      weatherCtx.beginPath();
-      weatherCtx.ellipse(
+      rainParticle.beginPath();
+      rainParticle.ellipse(
         raindrop.x,
         raindrop.y,
         raindrop.width,
@@ -235,18 +250,33 @@ if (window.location.href === targetCW3) {
         Math.PI,
         2 * Math.PI
       );
-      weatherCtx.fillStyle = "rgba(150, 150, 150, 0.4)";
-      weatherCtx.fill();
+      rainParticle.fillStyle = "rgba(150, 150, 150, 0.4)";
+      rainParticle.fill();
     }
-  
+
+    // Добавление canvas-элемента в контейнер. пипец
+    weatherContainer.appendChild(rainCanvas);
+
+    // Запуск анимации. блять где я
     animateRain();
-  
-    return { raindrops };
+
+    return {
+      rainCanvas,
+      rainParticle,
+      raindrops,
+    };
   }
   // ====================================================================================================================
+
+  // ====================================================================================================================
+  // Генератор котопада
   function generateSnowflakes() {
     const snowflakes = [];
-  
+    const snowCanvas = document.createElement("canvas");
+    snowCanvas.classList.add("snowCanvas");
+    const snowParticle = snowCanvas.getContext("2d");
+
+    // Интервал генерации новых снежинок.
     setInterval(() => {
       for (let i = 0; i < 1; i++) {
         const snowflake = weather === "snow" ? generateSnowflake() : null;
@@ -255,133 +285,88 @@ if (window.location.href === targetCW3) {
         }
       }
     }, 100);
-  
+
+    // Генерируем снежинку
     function generateSnowflake() {
       if (document.hidden) {
         return;
       }
       const y = Math.random() * -100;
-      const x = Math.random() * weatherCanvas.width;
+      const x = Math.random() * snowCanvas.width;
       const size = Math.random() * 5 + 2;
       const ySpeed = size * 0.14;
       const xSpeed = size * (Math.random() - Math.random()) * 0.02;
-  
+
       return { x, y, size, ySpeed, xSpeed };
     }
-  
+
+    // Анимация снежинки. Типа падают вниз.
     function animateSnow() {
       if (snowflakes.length > 0) {
-        weatherCtx.clearRect(0, 0, weatherCanvas.width, weatherCanvas.height);
+        snowParticle.clearRect(0, 0, snowCanvas.width, snowCanvas.height);
         for (const snowflake of snowflakes) {
           snowflake.y += snowflake.ySpeed;
           snowflake.x += snowflake.xSpeed;
-          drawSnowflake(snowflake.x, snowflake.y, snowflake.size);
+          drawSnowflake(snowParticle, snowflake.x, snowflake.y, snowflake.size);
         }
       }
       requestAnimationFrame(animateSnow);
     }
 
-    function drawSnowflake(x, y, size) {
-      weatherCtx.beginPath();
-      weatherCtx.ellipse(x, y, size, size, 0, 0, 2 * Math.PI);
-      weatherCtx.fillStyle = "white";
-      weatherCtx.fill();
+    // Функция для рисования снежинок.
+    function drawSnowflake(snowParticle, x, y, size) {
+      snowParticle.beginPath();
+      snowParticle.ellipse(x, y, size, size, 0, 0, 2 * Math.PI);
+      const color = "white";
+      snowParticle.fillStyle = color;
+      snowParticle.fill();
     }
-    
+
+    // Добавление canvas-элемента в контейнер. пипец
+    weatherContainer.appendChild(snowCanvas);
+
+    // Запуск анимации. блять где я
     animateSnow();
 
-    return { snowflakes };
+    // Возвращение объекта. нахуя правда.
+    return {
+      snowCanvas,
+      snowParticle,
+      snowflakes,
+    };
   }
   // ====================================================================================================================
-  function generateAurora() {
-    const auroras = [];
-    let currentHue = 180;
-
-    setInterval(() => {
-      for (let i = 0; i < 1; i++) {
-        const aurora = weather === "clear" ? generateAuroraParticle() : null;
-        if (aurora) {
-          auroras.push(aurora);
-        }
-      }
-      // console.log(`Количество элементов: ${auroras.length}`)
-    }, 1000);
-  
-    function generateAuroraParticle() {
-      if (document.hidden) {
-        return;
-      }
-      const y = weatherCanvas.height * 0.98;
-      const x = weatherCanvas.width * -0.04;
-      const size = Math.random() * 3 + 5;
-      const ySpeed = 0;
-      const xSpeed = Math.random() * 0.05 + 0.6;
-    
-      // Постепенно увеличиваем оттенок для каждой новой частицы
-      currentHue += 6;
-      if (currentHue > 270) {
-        currentHue = 180;
-      }
-    
-      const saturation = 100; // Насыщенность (интенсивность цвета)
-      const lightness = 50; // Яркость (светлость или темнота цвета)
-      const color = `hsl(${currentHue}, ${saturation}%, ${lightness}%)`;
-    
-      return { x, y, size, ySpeed, xSpeed, color };
-    }
-  
-    function animateAurora() {
-      if (auroras.length > 0) {
-        weatherCtx.clearRect(0, 0, weatherCanvas.width, weatherCanvas.height);
-        weatherCtx.filter = "blur(50px)";
-        for (let i = auroras.length - 1; i >= 0; i--) {
-          const aurora = auroras[i];
-          aurora.x += aurora.xSpeed;
-    
-          // Проверяем, вышла ли частица за пределы экрана
-          if (aurora.x > weatherCanvas.width * 1.2) {
-            auroras.splice(i, 1);
-            continue;
-          }
-    
-          drawAuroraParticle(aurora.x, aurora.y, aurora.size, aurora.color);
-        }
-        weatherCtx.filter = "none";
-      }
-      requestAnimationFrame(animateAurora);
-    }
-  
-    function drawAuroraParticle(x, y, size, color) {
-      weatherCtx.beginPath();
-      weatherCtx.ellipse(x, y, size * 5, size * 13, 0, 0, 2 * Math.PI);
-      weatherCtx.fillStyle = color;
-      weatherCtx.fill();
-    }
-  
-    animateAurora();
-  
-    return { auroras };
+  function resizeCanvasElement(canvas) {
+    canvas.width = canvas.parentNode.offsetWidth;
+    canvas.height = canvas.parentNode.offsetHeight;
   }
+
+  const { rainCanvas, rainParticle, raindrops } = generateRain();
+  resizeCanvasElement(rainCanvas);
+
+  const { snowCanvas, snowParticle, snowflakes } = generateSnowflakes();
+  resizeCanvasElement(snowCanvas);
+
+  window.addEventListener("resize", () => {
+    resizeCanvasElement(rainCanvas);
+    resizeCanvasElement(snowCanvas);
+  });
   // ====================================================================================================================
   function checkElements(elements, container) {
     for (let i = elements.length - 1; i >= 0; i--) {
       const element = elements[i];
-  
-      if (element && (element.y >= container.offsetHeight || element.x >= container.offsetWidth || element.x <= 0)) {
+
+      if (element && element.y >= container.offsetHeight) {
         elements.splice(i, 1);
       }
     }
-    // console.log(`Количество элементов: ${elements.length}`)
+    // console.log(`Количество элементов: ${elements.length}`);
   }
-  
+
   setInterval(() => {
-    checkElements(raindrops, weatherContainer);
     checkElements(snowflakes, weatherContainer);
+    checkElements(raindrops, weatherContainer);
   }, 100);
-  
-  const { auroras } = generateAurora();
-  const { raindrops } = generateRain();
-  const { snowflakes } = generateSnowflakes();
   // ====================================================================================================================
 }
 // ====================================================================================================================
