20df35dab8843411756355770a08aab5a82a81be
Ibi_Tea <hyvak52@gmail.com>
2024-11-15 14:06:01 +0300

    try some things

---
+++ modules/music.js
@@ -0,0 +1,21 @@
+setupSingleCallback("#cages_div", playMusic);
+
+function playMusic() {
+    return new Promise((resolve, reject) => {
+        soundManager.loadSound(
+            "notificationSound1",
+            "https://github.com/Ibirtem/CatWar/raw/main/sounds/notification_1.mp3",
+            (success) => {
+                if (success) {
+                    resolve();
+                } else {
+                    reject(new Error("Ошибка при загрузке звука"));
+                }
+            }
+        );
+    }).then(() => {
+        soundManager.playSound("notificationSound3", "5");
+    }).catch((error) => {
+        console.error("Ошибка:", error);
+    });
+}
