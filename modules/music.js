05e1837815d881799b84a82ebb573865784611e7
Ibi_Tea <hyvak52@gmail.com>
2024-11-15 14:38:24 +0300

    epupe

--- modules/music.js
+++ modules/music.js
@@ -4,22 +4,9 @@ setupSingleCallback("#cages_div", playMusic);
 
 function playMusic() {
     console.log("Ура, калбечек!");
-    return new Promise((resolve, reject) => {
         soundManager.loadSound(
             "notificationSound1",
             "https://github.com/Ibirtem/CatWar/raw/main/sounds/notification_1.mp3",
-            (success) => {
-                if (success) {
-                    resolve();
-                } else {
-                    reject(new Error("Ошибка при загрузке звука"));
-                }
-            }
         );
-    }).then(() => {
         soundManager.playSound("notificationSound3", "5");
-        console.error("Ура, звук!");
-    }).catch((error) => {
-        console.error("Ошибка:", error);
-    });
 }
