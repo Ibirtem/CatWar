45897597eb5f14077250c68cce901eceb68a16a8
Ibi_Tea <hyvak52@gmail.com>
2024-11-16 11:14:47 +0300

    Update music.js

--- modules/music.js
+++ modules/music.js
@@ -1,12 +1,9 @@
-console.log("Ура, модуль!");
-
 setupSingleCallback("#cages_div", playMusic);
 
 function playMusic() {
-    console.log("Ура, калбечек!");
         soundManager.loadSound(
-            "notificationSound1",
+            "mountsound",
             "https://github.com/Ibirtem/CatWar/raw/main/sounds/mountain-wind-rushing-wind.mp3",
         );
-        soundManager.playSound("notificationSound3", "5");
+        soundManager.playSound("mountsound", "5", true);
 }
